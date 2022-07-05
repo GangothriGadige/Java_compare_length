10b105f4b26887a804a8c121cb9d32f0f667f05f
Gangothri <gangothrigadige985@gmail.com>
2022-07-06 00:32:55 +0530

    added method to check one centimeter is equal to one centimeter

--- src/main/java/com/tw/Length.java
+++ src/main/java/com/tw/Length.java
@@ -1,4 +1,24 @@
 package com.tw;
 
+import java.util.Objects;
+
 public class Length {
+    private final int length;
+
+    Length(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Length length1 = (Length) o;
+        return length == length1.length;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(length);
+    }
 }
--- src/test/java/com/tw/LengthTest.java
+++ src/test/java/com/tw/LengthTest.java
@@ -1,4 +1,26 @@
 package com.tw;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.IsNot.not;
+
 public class LengthTest {
+
+    @Test
+    public void shouldCheckEqualityWhenOneCentimeterIsComparedWithOneCentimeter(){
+        Length firstLength = new Length(1);
+        Length secondLength = new Length(1);
+
+        assertThat(firstLength,equalTo(secondLength));
+    }
+
+    @Test
+    public void shouldCheckEqualityWhenTwoCentimeterIsComparedWithOneCentimeter(){
+        Length firstLength = new Length(2);
+        Length secondLength = new Length(1);
+
+        assertThat(firstLength, not(equalTo(secondLength)));
+    }
 }
